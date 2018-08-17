8bad4ff51edec932f2431b6d536e6b7d2e612f26
Brent Bauman <brentbauman86@gmail.com>
2018-08-17 17:41:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
-    @post = PigLatinizer.new(params[:text])
+  post '/PigLatinizer' do
+    word = params[:text])
 
     erb :user_input
   end
