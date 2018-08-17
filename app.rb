c409417cc240c5773c1cf11690410987f689e41d
Brent Bauman <brentbauman86@gmail.com>
2018-08-17 18:17:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/pigLatinizer' do
-    user_word = params[:word]
+    user_word = params[:user_phrase]
     word = PigLatinizer.new
     @pig_latinized_word = word.to_pig_latin(user_word)
     erb :pig_latin
