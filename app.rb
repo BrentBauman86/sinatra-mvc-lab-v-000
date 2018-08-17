28d28136eec8c6be9b40d3c5d6f2452a7886d357
Brent Bauman <brentbauman86@gmail.com>
2018-08-17 18:17:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/pigLatinizer' do
+  post '/pigLatinize' do
     user_word = params[:user_phrase]
     word = PigLatinizer.new
     @pig_latinized_word = word.to_pig_latin(user_word)
