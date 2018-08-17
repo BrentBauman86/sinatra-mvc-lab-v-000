8832c67efba992604414c1655df7f5de9b2ab150
Brent Bauman <brentbauman86@gmail.com>
2018-08-17 17:00:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/user_input' do
-    
+
     erb :user_input
   end
 
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
+    erb :results
+  end
 
 
 end
